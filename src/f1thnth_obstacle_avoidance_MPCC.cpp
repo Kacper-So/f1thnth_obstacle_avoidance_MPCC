e11d76686241d5c31a756fd95ad125d4697c4d76
Kacper-So <kacpersobczak152@gmail.com>
2024-06-08 13:21:39 +0200

    code cleanup

--- src/f1thnth_obstacle_avoidance_MPCC.cpp
+++ src/f1thnth_obstacle_avoidance_MPCC.cpp
@@ -1,66 +1,107 @@
 #include "rclcpp/rclcpp.hpp"
-#include "autoware_auto_msgs/msg/trajectory.hpp"
+#include "autoware_auto_planning_msgs/msg/trajectory.hpp"
+#include "autoware_auto_control_msgs/msg/ackermann_control_command.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/point_cloud2_iterator.hpp"
+#include "autoware_auto_vehicle_msgs/msg/velocity_report.hpp"
+#include "tf2/utils.h"
+#include <tf2/convert.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/ximgproc.hpp>
+#include <vector>
+#include <tuple>
+#include <queue>
+#include <unordered_map>
 
 using namespace std::placeholders;
 
-// Assume MPCC algorithm implementation here
-// You need to include the MPCC headers and define the necessary functions
+struct Cell {
+    int x;
+    int y;
+    bool operator<(const Cell& other) const {
+        return std::tie(x, y) < std::tie(other.x, other.y);
+    }
+    bool operator==(const Cell& other) const {
+        return x == other.x && y == other.y;
+    }
+};
 
-class ObstacleAvoidanceMPCC : public rclcpp::Node {
+class TrajSelector : public rclcpp::Node {
 public:
-    ObstacleAvoidanceMPCC() : Node("obstacle_avoidance_MPCC") {
+    TrajSelector() : Node("Traj_selector") {
         // Subscription to topics
-        racing_planner_traj_sub_ = this->create_subscription<autoware_auto_msgs::msg::Trajectory>(
+        racing_planner_traj_sub_ = this->create_subscription<autoware_auto_planning_msgs::msg::Trajectory>(
             "/planning/racing_planner/trajectory", 10,
-            std::bind(&ObstacleAvoidanceMPCC::racingPlannerTrajectoryCallback, this, _1));
+            std::bind(&TrajSelector::racingPlannerTrajectoryCallback, this, _1));
 
-        updated_map_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
-            "/updated_map", 10,
-            std::bind(&ObstacleAvoidanceMPCC::updatedMapCallback, this, _1));
+        astar_traj_sub_ = this->create_subscription<autoware_auto_planning_msgs::msg::Trajectory>(
+            "/planning/astar/trajectory", 10,
+            std::bind(&TrajSelector::astarCallback, this, _1));
 
         obstacle_detected_sub_ = this->create_subscription<std_msgs::msg::String>(
-            "/obstacle_detected", 10,
-            std::bind(&ObstacleAvoidanceMPCC::obstacleDetectedCallback, this, _1));
+            "/obstacle_alarm", 10,
+            std::bind(&TrajSelector::obstacleDetectedCallback, this, _1));
 
         // Publisher
-        avoidance_traj_pub_ = this->create_publisher<autoware_auto_msgs::msg::Trajectory>(
+        avoidance_traj_pub_ = this->create_publisher<autoware_auto_planning_msgs::msg::Trajectory>(
             "/planning/racing_planner/avoidance/trajectory", 10);
+
+        clock_ = this->get_clock();
+        last_callback_time_ = clock_->now();
     }
 
 private:
-    void racingPlannerTrajectoryCallback(const autoware_auto_msgs::msg::Trajectory::SharedPtr msg) {
-        // Set the goal point for MPCC algorithm based on the racing planner trajectory
-        // Implement your logic here
+    autoware_auto_planning_msgs::msg::Trajectory astar_traj;
+    float out_time = 1.;
+    bool obstacle_detected = false;
+    rclcpp::Time last_callback_time_;
+    double accumulated_time_ = 0.0;
+    rclcpp::Clock::SharedPtr clock_;
+    
+    void astarCallback(const autoware_auto_planning_msgs::msg::Trajectory::SharedPtr msg){
+        astar_traj = *msg;
     }
+    
+    void racingPlannerTrajectoryCallback(const autoware_auto_planning_msgs::msg::Trajectory::SharedPtr msg) {
+        rclcpp::Time current_time = clock_->now();
+        rclcpp::Duration duration = current_time - last_callback_time_;
+        double dt = duration.seconds();
+        accumulated_time_ += dt;
+        last_callback_time_ = current_time;
 
-    void updatedMapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
-        // If obstacle_detected_received is true, calculate trajectory using MPCC algorithm
-        if (obstacle_detected_received_) {
-            // Calculate trajectory using MPCC algorithm based on the updated map
-            // Implement your MPCC algorithm logic here
-            autoware_auto_msgs::msg::Trajectory avoidance_traj_msg;
-            // Fill avoidance_traj_msg with calculated trajectory
-            avoidance_traj_pub_->publish(avoidance_traj_msg);
+        trajectory_reference = *msg;
+        if (obstacle_detected) {
+            if (accumulated_time_ > out_time) {
+                obstacle_detected = false;
+                accumulated_time_ = 0.0;
+            } else {
+                avoidance_traj_pub_->publish(astar_traj);
+            }
+        } else {
+            avoidance_traj_pub_->publish(*msg);
         }
     }
 
     void obstacleDetectedCallback(const std_msgs::msg::String::SharedPtr msg) {
-        obstacle_detected_received_ = true;
-        // Implement your logic here if needed
+        obstacle_detected = true;
+        accumulated_time_ = 0.0;
+        last_callback_time_ = clock_->now();
     }
 
-    rclcpp::Subscription<autoware_auto_msgs::msg::Trajectory>::SharedPtr racing_planner_traj_sub_;
-    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr updated_map_sub_;
+    rclcpp::Subscription<autoware_auto_planning_msgs::msg::Trajectory>::SharedPtr racing_planner_traj_sub_;
+    rclcpp::Subscription<autoware_auto_planning_msgs::msg::Trajectory>::SharedPtr astar_traj_sub_;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr obstacle_detected_sub_;
-    rclcpp::Publisher<autoware_auto_msgs::msg::Trajectory>::SharedPtr avoidance_traj_pub_;
-    bool obstacle_detected_received_ = false;
+    rclcpp::Publisher<autoware_auto_planning_msgs::msg::Trajectory>::SharedPtr avoidance_traj_pub_;
 };
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<ObstacleAvoidanceMPCC>();
+    auto node = std::make_shared<TrajSelector>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
